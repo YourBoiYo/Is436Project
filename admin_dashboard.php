39a9b22822bc106f8719da4ec4d4a7c5f511050d
Yohan-git <38788515+YourBoiYo@users.noreply.github.com>
2019-12-02 00:22:41 -0500

    Add files via upload

---
+++ admin_dashboard.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+
+<head>
+  <meta charset="utf-8">
+  <<link rel="stylesheet" href="layout.css">
+  <title>Admin Dashboard</title>
+</head>
+
+<body>
+    <div class="dashboard">
+      <a href="results.php">Results</a>
+    </div>
+</body>
